8d4e4989ba61316079faf4255bbf622dc2a9a767
Petr Horacek <phoracek.edit@gmail.com>
2026-01-14 09:05:24 +0100

    many changes

--- tmac/msys_install.sh
+++ tmac/msys_install.sh
@@ -1,6 +1,3 @@
 #!bin/sh
 
-pacman -S mingw-w64-x86_64-texlive-core mingw-w64-x86_64-texlive-lang-czechslovak
-
-mingw-w64-x86_64-texlive-plain-generic
-mingw-w64-x86_64-texlive-fonts-recommended
+pacman -S mingw-w64-x86_64-texlive-core mingw-w64-x86_64-texlive-lang-czechslovak mingw-w64-x86_64-texlive-plain-generic mingw-w64-x86_64-texlive-fonts-recommended
